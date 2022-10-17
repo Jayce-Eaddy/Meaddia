1bc1ead3fd9e493df828f9e4ca7589041066a8d0
Jayce Eaddy <jaycemichaeleaddy@gmail.com>
2022-10-16 19:10:22 -0500

    Bug fixes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,8 @@
+import React from 'react';
 import { Stack } from '@mui/material';
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New';
-
-const Sidebar = () => (
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
     <Stack
         direction='row'
         sx={{
@@ -15,6 +14,7 @@ const Sidebar = () => (
         {categories.map((category) => (
             <button
                 className='category-btn'
+                onClick={() => setSelectedCategory(category.name)}
                 style={{
                     background: category.name ===
                         selectedCategory && '#7289da',
@@ -29,6 +29,6 @@ const Sidebar = () => (
             </button>
         ))}
     </Stack>
-)
+);
 
-export default Sidebar
+export default Categories;
